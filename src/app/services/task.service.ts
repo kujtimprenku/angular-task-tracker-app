31404b37fad10b664767ad2e327a7ecb1978697f
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2021-05-31 09:45:24 +0200

    Using http to fetch tasks from server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http'
 import { Task } from '../Task'
-import { TASKS } from '../mock-tasks'
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks (): Observable<Task[]> {
-    return of(TASKS)
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
