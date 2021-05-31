fde8be24b9a5919ab3e00137ccabcbc5d70963cc
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2021-05-31 11:55:09 +0200

    toggle form with subject observable

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs'
+import { UiService } from 'src/app/services/ui.service';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -8,12 +10,16 @@ import { Task } from 'src/app/Task';
 })
 export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
+  showAddTask: boolean = false
+  subscription: Subscription | undefined
 
   text: string = ''
   day: string = ''
   reminder: boolean = false
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value))
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs'
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title:string = 'Task Tracker';
-  constructor() { }
+  showAddTask: boolean = false
+  subscription: Subscription | undefined
+
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value))
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask () {
-    console.log('Toggle')
+    this.uiService.toggleAddTask()
   }
 }
